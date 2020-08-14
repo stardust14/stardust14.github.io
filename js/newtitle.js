8f88e8c07371790477c6b1a7a6ebab7d3148a886
stardust14 <1137094865@qq.com>
2020-08-14 20:28:48 +0800

    Site updated: 2020-08-14 20:28:39

---
+++ js/newtitle.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">    
+    <title>这是一个标题</title>
+</head>
+<body>
+<script type="text/javascript">    
+    window.onblur=function(){        
+        document.title="回来qwq";    
+        };    
+        window.onfocus=function () {        
+        document.title="这是一个标题";   
+        }
+        </script>
+</body>
+</html>
