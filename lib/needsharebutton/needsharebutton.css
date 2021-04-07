d29a3ba072a77323f7ffed383a6c116a3a72fff0
stardust14 <1137094865@qq.com>
2021-04-07 09:40:24 +0800

    Site updated: 2021-04-07 09:40:11

--- lib/needsharebutton/needsharebutton.css
+++ lib/needsharebutton/needsharebutton.css
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\stardust14.github.io\assets\css\APlayer.min.css"><script src="\stardust14.github.io\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>/***********************************************
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>/***********************************************
   needShareButton
   - Version 1.0.0
   - Copyright 2015 Dzmitry Vasileuski
